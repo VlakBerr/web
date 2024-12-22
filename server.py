1b058347f902c40953102527f0de3a834e7c24b7
VlakBerr <155465724+VlakBerr@users.noreply.github.com>
2024-12-22 13:17:14 +0300

    Добавили сервер

---
+++ server.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def ruwiki():
+    return render_template("ruwiki.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)

06648bf784529a79640393f66e2da19443032ad5
VlakBerr <boro46141@gmail.com>
2024-12-29 13:15:30 +0300

    добавил примеры запросов

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
@@ -6,6 +6,17 @@ app = Flask(__name__)
 @app.route("/")
 def ruwiki():
     return render_template("ruwiki.html")
+
+@app.route("/hello")
+def hello():
+    return 'привет мир'
+
+@app.route("/max")
+def find_max():
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+    if a > b: return f'{a}'
+    else: return f'{b}'
 
 if __name__ == '__main__':
     app.run(debug=True)
